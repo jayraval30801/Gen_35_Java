813c92d21200cd48300c9d84d7469ac7a6a6df2b
Jay Raval <jayraval30801@gmail.com>
2023-06-09 16:28:28 +0530

    Switch Statement

---
+++ SwitchStatement.java
@@ -0,0 +1,27 @@
+package Gen_35;
+
+import java.util.Scanner;
+
+public class SwitchStatement {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+//		System.out.println("Enter Value of a");
+//		a = sc.nextInt();
+//		switch(a)
+//		{
+//		case 1:
+//				System.out.println("Ferbuary");
+//				break;
+//		case 2: 
+//				System.out.println("January");
+//				break;
+//		default:
+//				System.out.println("Invalid Choice");
+//		}
+		/* Loops : */
+		
+	}
+
+}
